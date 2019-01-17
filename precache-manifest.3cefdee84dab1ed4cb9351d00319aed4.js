228fe05ce0fcb60ccfc5544cc9a927251e5b69a0
Mohamed Adel <Ov3rControl@hotmail.com>
2019-01-18 00:58:58 +0200

    Updates

---
+++ precache-manifest.3cefdee84dab1ed4cb9351d00319aed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4e0b30b0dbf2eb1695e",
+    "url": "/my-app/./static/js/runtime~main.d4e0b30b.js"
+  },
+  {
+    "revision": "7bf0e96c199f16ff0a0f",
+    "url": "/my-app/./static/js/main.7bf0e96c.chunk.js"
+  },
+  {
+    "revision": "640e2b635b5067af4b6c",
+    "url": "/my-app/./static/js/1.640e2b63.chunk.js"
+  },
+  {
+    "revision": "7bf0e96c199f16ff0a0f",
+    "url": "/my-app/./static/css/main.239ab4de.chunk.css"
+  },
+  {
+    "revision": "640e2b635b5067af4b6c",
+    "url": "/my-app/./static/css/1.a0d0e350.chunk.css"
+  },
+  {
+    "revision": "36e4383566146797c3a39c24c51b4a4a",
+    "url": "/my-app/./index.html"
+  }
+];
